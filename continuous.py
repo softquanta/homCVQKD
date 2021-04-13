b3a754c16c92845f5dc22aec3825cf6d2169a305
Kieran Wilkinson <kieranwilkinson1@gmail.com>
2021-04-13 14:16:04 +0200

    Update continuous.py
    Modification of symplectic eigenvalue calculation to support covariance matrices of any valid dimension. Includes introduction of general symplectic form function

--- continuous.py
+++ continuous.py
@@ -15,6 +15,18 @@
 
 import numpy as np
 
+def Omga(n):
+    """
+    Compute the symplectic form
+
+    Parameters
+    ----------
+    :param n: The number of modes in the system
+    :return: The symplectic form for the system
+    """ 
+
+    return np.kron(np.eye(int(n)), [[0, 1], [-1, 0]])
+
 
 def symplectic_eigenvalue_calculation(V):
     """
@@ -33,19 +45,29 @@ def symplectic_eigenvalue_calculation(V):
         Delta_V = np.linalg.det(A_mode) + np.linalg.det(B_mode) + 2 * np.linalg.det(C_mode)
         v_1 = np.sqrt((Delta_V + np.sqrt(Delta_V ** 2 - 4 * np.linalg.det(V))) / 2)
         v_2 = np.sqrt((Delta_V - np.sqrt(Delta_V ** 2 - 4 * np.linalg.det(V))) / 2)
+
+        # Assert that the eigenvalues are positive and larger than unit
+        assert v_1 > 1
+        assert v_2 > 1
+
+        return v_1, v_2
+
     else:
-        iOmega = np.dot(1j, np.array([[0, 1, 0, 0], [-1, 0, 0, 0], [0, 0, 0, 1], [0, 0, -1, 0]]))  # iΩ matrix
-        v = np.linalg.eigvals((np.dot(iOmega, V)))
-        v_1 = np.abs(v[0])
-        v_2 = np.abs(v[3])
+        Om = Omga(V.shape[0]/2)
+        eigsFull = np.linalg.eigvals(1j*np.dot(Om, V))
+        eigsSort = np.sort(abs(eigsFull))
+        symeigs = np.delete(eigsSort, [x for x in range(0, V.shape[0], 2)])
+        
+        assert np.all(symeigs > 1)
 
-    # Assert that the eigenvalues are positive and larger than unit
-    assert v_1 > 1
-    assert v_2 > 1
+        return tuple(symeigs)
 
-    return v_1, v_2
+        # iOmega = np.dot(1j, Omga(2))  # iΩ matrix
+        # v = np.linalg.eigvals((np.dot(iOmega, V)))
+        # v_1 = np.abs(v[0])
+        # v_2 = np.abs(v[3])
 
-
+ 
 def h_f(v):
     """
     A bosonic entropic function which calculates the Von Neumann entropy.
